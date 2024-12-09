7a25779fb1ca38147c4dde2fea817e181eccfe42
lgg <glissovskiy@gmail.com>
2024-12-09 13:18:52 -0800

    added entity customer

--- src/main/java/info/javalab/customer/Customer.java
+++ src/main/java/info/javalab/customer/Customer.java
@@ -1,8 +1,21 @@
 package info.javalab.customer;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
 public class Customer{
+
+    @Id
+    @SequenceGenerator(
+            name="customer_id_seq",
+            sequenceName = "customer_id_seq"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_seq"
+    )
     private Integer id;
     private String name;
     private String email;
