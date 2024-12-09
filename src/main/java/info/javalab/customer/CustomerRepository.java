f8d64e68de755cc7ec67fcc6c813d858fcf9c74d
lgg <glissovskiy@gmail.com>
2024-12-09 13:38:33 -0800

    added customer reporsitory

---
+++ src/main/java/info/javalab/customer/CustomerRepository.java
@@ -0,0 +1,7 @@
+package info.javalab.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
