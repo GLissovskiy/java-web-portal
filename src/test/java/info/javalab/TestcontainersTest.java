d4170593585b0da8ea52c3a9afb948769196e0c5
lgg <glissovskiy@gmail.com>
2024-12-13 10:13:04 -0800

    added dynamicPropertySource to test

--- src/test/java/info/javalab/TestcontainersTest.java
+++ src/test/java/info/javalab/TestcontainersTest.java
@@ -2,6 +2,8 @@ package info.javalab;
 
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -17,6 +19,14 @@ public class TestcontainersTest {
                     .withDatabaseName("customer-dao-unit-test")
                     .withUsername("postgres")
                     .withPassword("postgres");
+
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
