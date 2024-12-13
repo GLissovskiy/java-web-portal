70304c3512a5bef7b936575a394e51d74d73811b
lgg <glissovskiy@gmail.com>
2024-12-13 09:46:04 -0800

    added testContainers for psotgres

---
+++ src/test/java/info/javalab/TestcontainersTest.java
@@ -0,0 +1,26 @@
+package info.javalab;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public class TestcontainersTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:14")
+                    .withDatabaseName("customer-dao-unit-test")
+                    .withUsername("postgres")
+                    .withPassword("postgres");
+    @Test
+    void canStartPostgresDB() {
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+    }
+
+
+}
