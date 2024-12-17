863acaedfe64ebc509e53254cd9b2eff373261fd
lgg <glissovskiy@gmail.com>
2024-12-17 08:29:32 -0800

    added test for selectById

--- src/test/java/info/javalab/customer/CustomerJDBCDataAccessServiceTest.java
+++ src/test/java/info/javalab/customer/CustomerJDBCDataAccessServiceTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerJDBCDataAccessServiceTest extends AbstractTestcontainersUnitTest {
@@ -42,12 +44,43 @@ class CustomerJDBCDataAccessServiceTest extends AbstractTestcontainersUnitTest {
         List<Customer> customers = underTest.selectAllCustomers();
 
         //Then
-        Assertions.assertThat(customers).isNotEmpty();
+        assertThat(customers).isNotEmpty();
 
     }
 
     @Test
     void selectCustomerById() {
+
+        //Given
+
+        String email = FAKER.internet().safeEmailAddress()+ "-"+UUID.randomUUID();
+        Customer customer = new Customer(
+                FAKER.name().fullName(),
+                email,
+                20
+        );
+
+        underTest.insertCustomer(customer);
+
+        Integer id = underTest.selectAllCustomers()
+                .stream()
+                .filter(c -> c.getEmail().equals(email))
+                .map(c -> c.getId())
+                .findFirst()
+                .orElseThrow();
+
+        // When
+
+        Optional<Customer> actual = underTest.selectCustomerById(id);
+
+        // Then
+        assertThat(actual).isPresent().hasValueSatisfying( c->{
+            assertThat(c.getId()).isEqualTo(id);
+            assertThat(c.getEmail()).isEqualTo(customer.getEmail());
+            assertThat(c.getName()).isEqualTo(customer.getName());
+            assertThat(c.getAge()).isEqualTo(customer.getAge());
+        });
+
     }
 
     @Test
