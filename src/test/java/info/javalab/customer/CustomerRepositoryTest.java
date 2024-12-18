d909cec7b8f24156e072f650638ee6bf59ad42a6
lgg <glissovskiy@gmail.com>
2024-12-18 14:14:46 -0800

    added abstractTestContainer

--- src/test/java/info/javalab/customer/CustomerRepositoryTest.java
+++ src/test/java/info/javalab/customer/CustomerRepositoryTest.java
@@ -1,16 +1,21 @@
 package info.javalab.customer;
 
+import info.javalab.AbstractTestcontainersUnitTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
-@SpringBootTest // load application context with a bunch of beans we do not need
-class CustomerRepositoryTest {
+ //@SpringBootTest // load application context with a bunch of beans we do not need (268 beans to test simple method)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CustomerRepositoryTest extends AbstractTestcontainersUnitTest {
 
     @Autowired // equals to the public constructor of the class with class as parameter
     private CustomerRepository underTest;
